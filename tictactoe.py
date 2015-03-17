5ad00674ace226f2e7b94064e41435f356e38639
Nicole Lehrer <nicole.lehrer@gmail.com>
2015-03-17 17:51:15 -0500

    added cat's game - waits until no moves left to announce, could annouce earlier - todo later

--- tictactoe.py
+++ tictactoe.py
@@ -62,10 +62,10 @@ def checkGrid():
     if lDiagResult or rDiagResult == 1:
         return 1
 
-def checkIfFirstMove():
+def noMovesLeft():
     for countRow in range(0,3):
         for countCol in range(0,3):
-            if gridSpots[countRow][countCol] != " - ": #really bad need to take formatting out of variable
+            if gridSpots[countRow][countCol] == " - ": #really bad need to take formatting out of variable
                 return False
     return True
 
@@ -86,6 +86,11 @@ while winner == 0:
     if checkGrid() == 1:
         updateDisplay()
         print('\nWINNER IS' + currentPlayer + "!!! END OF GAME")
+        winner = 1
+
+    if noMovesLeft():
+        updateDisplay()
+        print('\nNO WINNER. END OF GAME')
         winner = 1
    
     if shouldUpdatePlayer:
